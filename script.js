01eefa62d11d6fb0df982594deba677300bf428b
Daniel Abdulai <danielabdulai@Daniels-MacBook-Pro.local>
2025-01-05 21:55:29 -0800

    dynamic creation of list items done

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ addModalCancel.addEventListener("click", function(){
 })
 
 addModalSubmit.addEventListener("click", function(){
-    const title = document.getElementById("title").value;
+    let title = document.getElementById("title").value;
 
-    const descriptions = document.getElementsByName("description");
+    let descriptions = document.getElementsByName("description");
     let descriptionValue = '';
     for (const description of descriptions) {
         if (description.checked) {
@@ -23,9 +23,9 @@ addModalSubmit.addEventListener("click", function(){
         }
     }
 
-    const date = document.getElementById("date").value;
+    let date = document.getElementById("date").value;
 
-    const priorities = document.getElementsByName("priority");
+    let priorities = document.getElementsByName("priority");
     let priorityValue = '';
     for (const priority of priorities) {
         if (priority.checked) {
@@ -38,5 +38,45 @@ addModalSubmit.addEventListener("click", function(){
         alert("Fill in all info")
     }else {
         alert("Item added");
+        displayItem(title, descriptionValue, date, priorityValue);
+        document.getElementById("title").value = "";
+        for (const description of descriptions) {
+            description.checked = false;
+        }
+        document.getElementById("date").value = "";
+        for (const priority of priorities) {
+            priority.checked = false;
+        }
+        addModal.style.display = "none";
       }
 })
+
+function displayItem(title, descriptionValue, date, priorityValue){
+    let body = document.getElementById("body");
+    let item = document.createElement("div");
+    let colorPhrase = '';
+    if(priorityValue == "high"){
+        colorPhrase = '<span style="height: 20px; width: 20px; background-color: red;"></span>';
+    }
+    else if(priorityValue == "medium"){
+        colorPhrase = '<span style="height: 20px; width: 20px; background-color: orange;"></span>';
+    }
+    else{
+        colorPhrase = '<span style="height: 20px; width: 20px; background-color: green;"></span>';
+    }
+    item.style.display = "flex";
+    item.style.alignItems = "center";
+    item.style.justifyContent = "space-between";
+    item.innerHTML = 
+        '<input type="checkbox" name="done" id="done">'+
+        '<h2 style="margin-right: auto; margin-left: 10px;">' + title + '</h2>'+
+        '<div style="display: flex; align-items: center; gap: 10px;">'+
+        colorPhrase +
+        '<p>' + date + '</p>'+
+        '<i class="fa-solid fa-circle-info"></i>'+
+        '<i class="fa-regular fa-pen-to-square"></i>'+    
+        '<i class="fa-solid fa-trash"></i>';
+
+    body.appendChild(item);
+}
+
